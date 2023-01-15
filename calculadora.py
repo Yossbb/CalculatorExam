9740d46fb4dc46231380d8749ce47036aa6ae025
yoss <yocelin.vazquez@elektra.com.mx>
2023-01-15 02:03:07 -0600

    creando menu...

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,13 @@ import numpy as np
 
 def calculator():
     print("hola mundo")
+    print("Menu de operaciones:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Raiz Cuadrada")
+    print("6. Exponente")
+    print("0. Salir")
 
 calculator()
